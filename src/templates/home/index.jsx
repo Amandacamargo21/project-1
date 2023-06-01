11055240e93c875fd6214e1fbf67df756d64e972
Amandacamargo21 <amandacamargo2102@gmail.com>
2023-06-01 18:45:30 -0300

    ok

--- src/templates/home/index.jsx
+++ src/templates/home/index.jsx
@@ -6,6 +6,7 @@ import './styles.css'
 import { loadPosts } from '../../utils/load-posts'
 import { Posts } from '../../components/Posts'
 import { Button } from '../../components/Button'
+import { TextInput } from '../../components/TextInput'
 
 class Home extends Component {
   state = {
@@ -55,13 +56,13 @@ class Home extends Component {
 
     return (
       <section className="container">
-        {!!searchValue && (
-          <>
-            <h1>Search value: {searchValue}</h1>
-          </>
-        )}
-        <br /> <br />
-        <input onChange={this.handleChange} value={searchValue} type="search" />
+        <div className="search-container">
+          {!!searchValue && <h1>Search value: {searchValue}</h1>}
+          <TextInput
+            searchValue={searchValue}
+            handleChange={this.handleChange}
+          />
+        </div>
         <br /> <br /> <br />
         {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
         {filteredPosts.length === 0 && <p>Não existem posts =(</p>}
